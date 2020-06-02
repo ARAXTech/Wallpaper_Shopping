c050386ad592eb778df654613dea0de9cc06d3fd
farzaneHajipour <d.hajipour@gmail.com>
2020-06-02 22:30:49 +0430

    Billing, Shipping & Order Added(99.03.13).

---
+++ app/src/main/java/Model/Billing.java
@@ -0,0 +1,135 @@
+
+package Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Billing {
+
+    @SerializedName("first_name")
+    @Expose
+    private String firstName;
+    @SerializedName("last_name")
+    @Expose
+    private String lastName;
+    @SerializedName("address_1")
+    @Expose
+    private String address1;
+    @SerializedName("address_2")
+    @Expose
+    private String address2;
+    @SerializedName("city")
+    @Expose
+    private String city;
+    @SerializedName("state")
+    @Expose
+    private String state;
+    @SerializedName("postcode")
+    @Expose
+    private String postcode;
+    @SerializedName("country")
+    @Expose
+    private String country;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+
+    public Billing(String firstName, String lastName, String address1, String city, String state, String postcode, String country, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address1 = address1;
+        this.city = city;
+        this.state = state;
+        this.postcode = postcode;
+        this.country = country;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Billing() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry() {
+        this.country = "ایران";
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
